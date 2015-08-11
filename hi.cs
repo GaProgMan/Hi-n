5cd79be6fb70b270c5b42475ee579b6b4038a2eb
Jamie Taylor <jamie@gaprogman.com>
2015-08-11 19:29:30 +0100

    Added Hi-n listing in C#

---
+++ hi.cs
@@ -0,0 +1,25 @@
+namespace HiNewline
+{
+   public class HiNewLine
+   {
+      public static void Main(string[] args)
+      {
+         for (var i = 0; i < 98; i++)
+         {
+            var eatPie = "eat pie";
+         }
+
+         var counter = 1;
+         var x = "a";
+         while (counter <= 999999)
+         {
+            x += "a";
+            counter++;
+         }
+
+         var squiggley = System.Math.Sqrt(5);
+
+         System.Console.Write("Hi\n");
+      }
+   }
+}
