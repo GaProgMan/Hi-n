09080a9082842818f1da15b304d769b0c30af404
Jamie Taylor <jamie@gaprogman.com>
2015-08-11 21:26:48 +0100

    Added Hi listing in C++

---
+++ hi.cpp
@@ -0,0 +1,24 @@
+#include <math.h>
+
+using namespace std;
+
+int main () {
+	string eat_pie;
+	int i = 0;
+	for (i; i < 98; i++) {
+		eat_pie = "eat pie";
+	}
+
+	int count = 0;
+	string x;
+	while (count < 999999) {
+		x += "a"
+		count++;
+	}
+
+	double squiggly = sqrt(5);
+
+	cout << "Hi\n" << endl;
+
+	return 0;
+}
