741fbc62730f6132c41e1c2200818a7b1e2cad6c
Christoph Kepler <development@kepler.international>
2016-10-04 17:57:54 +0200

    Add hi\n in C

---
+++ hi.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main () {
+	char eat_pie[7] = "eat pie";
+	int i = 1;
+	for (i; i < 98; i++) {
+		strcpy(eat_pie, "eat pie");
+	}
+
+	int count = 1;
+	char x[1000000] = "a";
+	while (count < 999999) {
+		strcat(x,"a");
+		count++;
+	}
+
+	double squiggly = sqrt(5);
+
+	printf("Hi\n");
+
+	return 0;
+}
