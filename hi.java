045685c03fa7d2fd640ed89adf971c913133eb32
Christoph Kepler <development@kepler.international>
2016-10-04 18:27:00 +0200

    Add hi\n in Java

---
+++ hi.java
@@ -0,0 +1,22 @@
+
+
+public class Hi_n {
+	public static void main (String[] args) {
+
+		String eat_pie;
+		for (int i = 0; i < 98; i++) {
+			eat_pie = "eat pie";
+		}
+
+		int count = 1;
+		String x = "a";
+		while (count < 999999) {
+			x += "a";
+			count++;
+		}
+
+		double squiggly = Math.sqrt(5);
+
+		System.out.print("Hi\n");
+	}
+}
